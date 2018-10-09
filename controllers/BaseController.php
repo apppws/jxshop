0a8b0ccbfc6eae0698d829431f955b43ff5fd5ac
pws <1771134069@qq.com>
2018-10-09 16:05:55 +0800

    权限访问

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -7,6 +7,14 @@ class BaseController{
         if(!isset($_SESSION['id'])){
             redirect('/login/login');
         }
+        // 获取要访问的路径
+        $path = isset($_SERVER['PATH_INFO']) ? trim($_SERVER['PATH_INFO'],'/') : 'index/index';
+        //设置白名单 
+        $wpath = ['index/index','index/top','index/menu','index/main'];
+        // 判断是否有权访问 
+        if(!in_array($path,array_merge($wpath,$_SESSION['url_path']))){
+            die('无权访问');
+        }
     }
 }
 ?>
--- models/Admin.php
+++ models/Admin.php
@@ -38,8 +38,18 @@ class Admin extends Model
         }
     }
 
+    // 删除
+    protected function _before_delete()
+    {
+        $stmt = $this->_db->prepare("DELETE FROM admin_role WHERE admin_id = ?");
+        $stmt->execute([
+            $_GET['id']
+        ]);
+    }
+
     // 登录注册
-    public function login($username,$password){
+    public function login($username, $password)
+    {
         // 预处理
         $stmt = $this->_db->prepare("SELECT * FROM admin WHERE username=? AND password=?");
         // 执行
@@ -50,15 +60,51 @@ class Admin extends Model
         // 取数据
         $info = $stmt->fetch(\PDO::FETCH_ASSOC);
         // 判断是否存在 并保存到sessin 中
-        if($info){
+        if ($info) {
             $_SESSION['id'] = $info['id'];
             $_SESSION['username'] = $info['username'];
-        }else{
+            $_SESSION['url_path'] = $this->getUalPath($_SESSION['id']);
+            // var_dump($_SESSION['url_path']);
+            // exit;
+        } else {
             throw new \Exception('用户名或者密码错误');
         }
     }
     // 退出登录
-    public function logout(){
+    public function logout()
+    {
         $_SESSION = [];
+    }
+
+    // 获取一个管理员有权访问的路径  
+    // 参数 用户id
+    public function getUalPath($adminId)
+    {
+        $sql = "SELECT c.url_path 
+                FROM admin_role a 
+                LEFT JOIN role_privlege b ON a.role_id  = b.role_id
+                LEFT JOIN privilege c ON b.pri_id=c.id
+                WHERE a.admin_id=? AND c.url_path!=''";
+        // 预处理
+        $stmt = $this->_db->prepare($sql);
+        $stmt->execute([$adminId]);
+        // 取数据
+        $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        // var_dump($data);
+        // 把二维数组 转成一维数组
+        $_ret = [];
+        foreach ($data as $v) {
+            // 判断是否有多个url_path
+            if (FALSE === strpos($v['url_path'], ',')) {
+                // 如果没有直接把那个地址拿来用
+                $_ret[] = $v['url_path'];
+            } else {
+                // 如果有逗号合并
+                $_t = explode(',', $v['url_path']);
+                $_ret = array_merge($_ret, $_t);
+            }
+        }
+
+        return $_ret;
     }
 }
